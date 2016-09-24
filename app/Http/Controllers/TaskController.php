e3cf848ebdfc5c526d14213c96c9df9a02f46744
jaya1995 <jayadey03@gmail.com>
2016-09-24 16:29:27 +0530

    Changes in taskcontroller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,22 +1,48 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Auth;
+use DB;
+use Redirect;
+use Input;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
 class TaskController extends Controller
 {
+	public function __construct()
+   {
+        $this->middleware('auth');
+        $this->user_id = Auth::user()->id;
+        $this->curr_timestamp = strtotime(date("Y-m-d H:i:s"));
+   }
     //show task insertion form
     public function index()
     {
     	return view('task.new');
-    }
-    public function saveTask(Requst $requst )
-{
-	$title=$request->input('title');
-	$description=$request->input('description');
-
+    } 
+	public function saveTask(Request $request)
+	{
+		try{
+		DB::table('schedule')->insert(
+                        [   'title' => trim($request->input('title')),
+                        	'description' => trim($request->input('description')),
+                        	'start_time' => trim($request->input('start_time')),
+                        	'end_time' => trim($request->input('end_time')),
+                            'created_by' => $this->user_id,
+                            'auto_id' => uniqid(),
+                            'updated_at' => date("Y-m-d H:i:s")
+                        ]
+                    );
+		$status = TRUE;
+	}catch(\Exception $e){
+                    $status = FALSE; 
+            }
+     if($status){
+     	return Redirect('task');
+     }
+     else{
+     	return Redirect('task');
+     }
 	}
 }
